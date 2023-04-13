f16f86b96412d67ea669bc91dcc9a2e0f5775bc6
Tinhtinh99 <nvva0907@gmail.com>
2023-04-13 21:31:19 +0700

    update

--- src/main/java/com/learning/apps/exceptions/HandleException.java
+++ src/main/java/com/learning/apps/exceptions/HandleException.java
@@ -6,22 +6,21 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.Collections;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class HandleException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     @ResponseBody
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public CustomResponse<?> badRequestException(BadRequestException ex) {
         try {
             return new CustomResponse<>(null, ex.getMessages(), String.valueOf(HttpStatus.BAD_REQUEST.value()));
@@ -32,6 +31,7 @@ public class HandleException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseBody
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public CustomResponse<?> handleRuntimeException(RuntimeException ex, HttpServletResponse response) {
         ex.printStackTrace();
         response.setStatus(HttpStatus.BAD_REQUEST.value());
@@ -39,8 +39,8 @@ public class HandleException extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public CustomResponse<?> handleMaxSizeException(MaxUploadSizeExceededException exc) {
         return new CustomResponse<>(null, Collections.singletonList("File too large!"), String.valueOf(HttpStatus.EXPECTATION_FAILED.value()));
-//        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File too large!"));
     }
 }
